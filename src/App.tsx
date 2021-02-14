9b8e2002ce79f9b11e11a096237120cedced0fc3
alimtegar <alimtegar4@gmail.com>
2021-02-14 09:40:16 +0700

    Create OCR, signature pad, and recognize signature pad writing with OCR

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Tesseract, { createWorker } from 'tesseract.js';
+import SignatureCanvas from 'react-signature-canvas'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type WorkerLog = {
+    workerId: string,
+    jobId?: string,
+    status?: string,
+    progress: number,
+};
+
+const App = () => {
+    // Refs
+    const [canvasRef, setCanvasRef] = useState<SignatureCanvas | null>(null);
+    const [resultText, setResultText] = useState<string>('');
+
+    const worker = createWorker({
+        logger: (m: WorkerLog) => console.log(m)
+    });
+
+    const recognize = async (imgUrl: string) => {
+        await worker.load();
+        await worker.loadLanguage('eng');
+        await worker.initialize('eng');
+        const { data: { text } }: Tesseract.RecognizeResult = await worker.recognize(imgUrl);
+
+        console.log(text);
+        setResultText(text);
+
+        await worker.terminate();
+    };
+
+    return (
+        <div className="App">
+            <SignatureCanvas
+                penColor='green'
+                canvasProps={{ width: 500, height: 200, className: 'sigCanvas' }}
+                ref={(ref) => setCanvasRef(ref)}
+            />
+
+            {/* {canvasRef && canvasRef.toDataURL()} */}
+
+            {/* {() => recognize(canvasRef.toDataURL())} */}
+            <button {...canvasRef ? { onClick: () => recognize(canvasRef.toDataURL()), } : { disabled: true, }} >
+                recognize
+            </button>
+
+            <textarea value={resultText} /> 
+        </div>
+    );
 }
 
 export default App;
