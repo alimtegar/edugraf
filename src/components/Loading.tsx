d72bf0ad1ea509f2befad627ab7dc58c5a8f8e12
alimtegar <alimtegar4@gmail.com>
2021-04-26 19:23:40 +0700

    Update logo

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,8 +1,15 @@
 import Logo from './Logo';
+import BarLoader from 'react-spinners/BarLoader';
+
 
 const Loading = () => (
     <main className="flex justify-center items-center flex-grow text-white">
-        <Logo />
+        <div className="flex flex-col justify-center items-center">
+            <div className="mb-2">
+                <Logo h={8} />
+            </div>
+            <BarLoader color="#ffffff" height={3} />
+        </div>
     </main>
 );
 
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,11 @@
-const Logo = () => (
+type Props = {
+    h?: number,
+};
+
+const Logo = ({ h = 6 }: Props) => (
     <h1 className="font-handwriting font-semibold text-2xl">
-        Sibisa
+        {/* Sibisa */}
+        <img src="/logo.svg" alt="Sibisa" className={`w-auto h-${h}`} />
     </h1>
 );
 
